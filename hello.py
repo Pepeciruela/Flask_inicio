10247944278bbf229c34346ae9329dcb7581b2bb
Angel <calvo.monzon.angel@gmail.com>
2020-12-02 17:41:15 +0100

    pruebas

--- hello.py
+++ hello.py
@@ -1,8 +1,11 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
-@app.route("/hello")
-
+@app.route("/")
 def hello_world():
-    return "Hola, mundo"
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug = True, port = 8000)
